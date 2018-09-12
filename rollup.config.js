b56eddbb45113b13ac92b11852426707bb016677
Shinji Yamaguchi <mag4ndnp@gmail.com>
2018-09-12 15:32:28 +0900

    ファイル名を変更

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,7 @@ import babel from 'rollup-plugin-babel'
 
 export default {
   entry: path.join(__dirname, './src/js/main.js'),
-  dest: path.join(__dirname, './dist/js/webgl-inset.js'),
+  dest: path.join(__dirname, './dist/js/wg-inset.js'),
   format: 'umd',
   plugins: [
     nodeResolve({ jsnext: true }), // npmモジュールを`node_modules`から読み込む
