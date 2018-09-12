800e564f1674bc67644be351cd86aac150dd0787
Shinji Yamaguchi <mag4ndnp@gmail.com>
2018-09-12 15:24:08 +0900

    設定ファイルを追加

---
+++ rollup.config.js
@@ -0,0 +1,17 @@
+import path from 'path'
+// import license from 'rollup-plugin-license'
+import nodeResolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import babel from 'rollup-plugin-babel'
+
+export default {
+  entry: path.join(__dirname, './src/js/main.js'),
+  dest: path.join(__dirname, './dist/js/webgl-inset.js'),
+  format: 'umd',
+  plugins: [
+    nodeResolve({ jsnext: true }), // npmモジュールを`node_modules`から読み込む
+    commonjs(), // CommonJSモジュールをES6に変換
+    babel() // ES5に変換
+  ],
+  external: ['jquery']
+}
